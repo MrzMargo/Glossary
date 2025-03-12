2bf9e8b720e8c51e83a8a2526df1082288580225
MrzMargo <106422831+MrzMargo@users.noreply.github.com>
2025-03-12 08:44:26 +0300

    creating files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function viewTerms() {
+    alert("Показать список всех терминов");
+}
+
+function addTerm() {
+    alert("Добавить новый термин");
+}
+
+function updateTerm() {
+    alert("Обновить термин");
+}
+
+function deleteTerm() {
+    alert("Удалить термин");
+}
